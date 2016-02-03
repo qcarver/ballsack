b0d736c3b9af25347650b0340ea8e7273c27f71e
Quinn Carver <qcarver@gmail.com>
2016-02-02 22:14:51 -0500

    added some comments as reminders on what to work on

--- src/main/java/me/qcarver/ballsack/Radians.java
+++ src/main/java/me/qcarver/ballsack/Radians.java
@@ -23,4 +23,22 @@ public class Radians{
     }
 
     //TODO isCcwOf... isCwOf
+    
+        //if ccwTanLineAngleToCenter is ccw of plantedCircles cw tangent
+                    //flag need to skooch clockwise
+                    //note ccw coliding circle.. if there is more than one, only consider nearest one
+                //if cwTanLineAngleToCenter is cw of planteCircles ccw tangent
+                    //flag needs to skooch counterclockwise
+                    //note cw coliding circle .. if there is more than one, only consider nearest one
+                //if !flags and !(colliding circles)
+                    //drop circle tangent to biggest (as before)
+                //if ccw flag and 1 colliding cicle 
+                    //skooch cirlce cw to angle tanget with colliding cirlces cw angle from center
+                //if cw flag and 1 colliding circle
+                    //skooch circle ccw to angle tangent with colliding circles ccw angle from center
+                //if both flags and one colliding circle
+                    //this is a smaller circle whch will rest on top of a larger one
+                //if both flags and two colliding circles
+                    //circle will rest on top of colliding circles 
+                    //(you have known side lenths of the triangle)
 }
